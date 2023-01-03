7262cb19a6a4319a231c63f9d7c12fbbcdd0ec35
GhostCodeKC <andrewmgrube@gmail.com>
2023-01-03 17:54:26 -0600

    init with coloring

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,23 @@
-import Link from 'next/link';
+import Link from "next/link";
+var copyrightYear = new Date().getFullYear();
 
 export default function Layout({ children }) {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <header className='bg-fuchsia-100 mb-8 py-4'>
-        <div className='container mx-auto flex justify-center'>
-          <Link href='/'>
-            <a>🏡</a>
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-emerald-800 mb-8 py-4">
+        <div className="container mx-auto flex justify-center">
+          <Link href="/">
+            <a>Home</a>
           </Link>
-          <span className='mx-auto'>Welcome to my blog</span>{' '}
+          <span className="mx-auto">Blog</span>{" "}
         </div>
       </header>
-      <main className='container mx-auto flex-1'>{children}</main>
-      <footer className='bg-fuchsia-100 mt-8 py-4'>
-        <div className='container mx-auto flex justify-center'>
-          &copy; 2022 DailyDevTips
+      <main className="container mx-auto flex-1">{children}</main>
+      <footer className="bg-emerald-800 mt-8 py-4">
+        <div className="container mx-auto flex justify-center">
+          <a href="https://andrewgrube.com">
+            &copy; {copyrightYear} Andrew Grube
+          </a>
         </div>
       </footer>
     </div>
